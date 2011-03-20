c108414f0fa7595740738f9e0ef2591277d38c7a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-20 13:41:49 +0100

    stuff

---
+++ src/common/Connection.cpp
@@ -0,0 +1,9 @@
+//
+//  Connection.cpp
+//  RemoteControl
+//
+//  Created by Albert Zeyer on 20.03.11.
+//  Copyright 2011 Albert Zeyer. All rights reserved.
+//
+
+#include "Connection.h"
---
+++ src/common/Connection.h
@@ -0,0 +1,13 @@
+//
+//  Connection.h
+//  RemoteControl
+//
+//  Created by Albert Zeyer on 20.03.11.
+//  Copyright 2011 Albert Zeyer. All rights reserved.
+//
+
+#ifndef __RemoteControl__Connection__
+#define __RemoteControl__Connection__
+
+
+#endif

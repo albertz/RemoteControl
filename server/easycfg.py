0ef188a15f79235d6c345a60746cf6bb2d0befd8
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-06-11 04:16:46 +0200

    easycfg

--- server/easycfg.py
+++ server/easycfg.py
@@ -2,8 +2,29 @@
 # code by Albert Zeyer, www.az2000.de
 # 2012-06-08
 
-def setup(vars, varnames):
-	pass
+CfgFile = None
+cfg = {}
 
+def setup(cfgfile, vars, varnames):
+	global cfg, CfgFile
+	CfgFile = cfgfile
+	for vn in varnames:
+		cfg[vn] = vars[vn]
+
+def betterRepr(o):
+	# the main difference: this one is deterministic
+	# the orig dict.__repr__ has the order undefined.
+	if isinstance(o, list):
+		return "[" + ", ".join(map(betterRepr, o)) + "]"
+	if isinstance(o, tuple):
+		return "(" + ", ".join(map(betterRepr, o)) + ")"
+	if isinstance(o, dict):
+		return "{\n" + "".join(map(lambda (k,v): betterRepr(k) + ": " + betterRepr(v) + ",\n", sorted(o.iteritems()))) + "}"
+	# fallback
+	return repr(o)
+	
 def save():
-	pass
+	global cfg, CfgFile
+	f = open(CfgFile, "w")
+	f.write(betterRepr(cfg))
+	f.write("\n")

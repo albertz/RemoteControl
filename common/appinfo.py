924a9ae316076913bff4a408156dd194f05b69ee
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-06-15 04:35:29 +0200

    appinfo

---
+++ common/appinfo.py
@@ -0,0 +1,17 @@
+progname = "RemoteEverywhere"
+appid = "com.albertzeyer." + progname
+version = "1.0"
+
+userdir = "~/." + progname
+
+if sys.platform == "darwin":
+	userdir = "~/Library/Application Support/" + progname
+elif sys.platform == "win32":
+	from win32com.shell import shellcon, shell
+	userdir = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0) + "/" + progname
+else:
+	raise Exception, "missing support for your platform"
+
+userdir = os.path.expanduser(userdir)
+try: os.makedirs(userdir)
+except: pass

2913619855e51d550a52d17813ca7ed7d26c0d0a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-06-23 05:46:46 +0200

    appinfo

--- common/appinfo.py
+++ common/appinfo.py
@@ -5,14 +5,21 @@ appid = "com.albertzeyer." + progname
 version = "1.0"
 
 userdir = "~/." + progname
+platform = None
 
-if sys.platform == "darwin":
+uname = os.uname()
+if "iPhone" in uname[4] or "iPod" in uname[4]:
+	userdir = "~/Library/" + progname
+	platform = "iOS"	
+elif sys.platform == "darwin":
 	userdir = "~/Library/Application Support/" + progname
+	platform = "MacOSX"
 elif sys.platform == "win32":
 	from win32com.shell import shellcon, shell
 	userdir = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0) + "/" + progname
+	platform = "Win32"
 else:
-	raise Exception, "missing support for your platform " + repr(sys.platform)
+	print "warning: unknown platform", repr(sys.platform)
 
 userdir = os.path.expanduser(userdir)
 try: os.makedirs(userdir)
